e1edc0f6d640ec4ea937ded5be07c516b0ba30a3
Karl Hedayati <k.hedayati.10@gmail.com>
2022-11-19 15:52:43 -0500

    params and hidden tag

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -5,7 +5,8 @@ class GamesController < ApplicationController
   def new
     @letters = 10.times.map { [*'a'..'z'].sample }
     # @letters = %w[h e l l o]
-    session[:letters] = @letters
+    # @letters = %w[w a g o n]
+    # session[:letters] = @letters
   end
 
   def letters?(string, grid)
@@ -18,14 +19,14 @@ class GamesController < ApplicationController
     url = "https://wagon-dictionary.herokuapp.com/#{params[:word]}"
     word_serialized = URI.open(url).read
     word = JSON.parse(word_serialized)
-    @letters = session[:letters]
+    # @letters = session[:letters] OR   <%= hidden_field_tag :letters, @letters %> in new.html.erb
+    # USE @letters                            params[:letters]
 
     if word['found']
       # Verify if all letters in attempt are in generate_grid(grid)
       @message = "Congratulations! #{params[:word]} is a valid English word!"
-      if !letters?(params[:word], @letters)
-        @message = "Sorry but #{params[:word]} can't be built out of #{@letters}."
-      end
+    elsif letters?(params[:word], params[:letters]) == false
+      @message = "Sorry but #{params[:word]} can't be built out of #{@letters}."
     else
       @message = "Sorry but #{params[:word]} does not seem to be a valid English word."
     end
